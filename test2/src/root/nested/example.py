313014be3515a761f7ec3da6e0fee48ba58cc3ec
Yaron <ymelech@gmail.com>
2016-04-10 00:23:38 +0300

    First commit

---
+++ test2/src/root/nested/example.py
@@ -0,0 +1,15 @@
+'''
+Created on 9 Apr 2016
+
+@author: Work
+'''
+
+if __name__ == '__main__':
+    x = 1
+    x = 2
+    x = 3
+    y = x * 4
+    print('Hello World')
+    print(x)
+    print(y)
+    pass
